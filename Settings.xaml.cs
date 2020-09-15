9e40aa3cb06f7d1249e9288be916e4f6ad86a646
LimKyeongJoon <kyeongjoon16881688@gmail.com>
2020-09-15 17:57:51 +0900

    폼 생성
    로그인, 메인, 설정, 위젯 화면 추가

--- Settings.xaml.cs
+++ Settings.xaml.cs
@@ -3,11 +3,11 @@
 namespace Maeily_Windows
 {
     /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// Settings.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Settings : Window
     {
-        public MainWindow()
+        public Settings()
         {
             InitializeComponent();
         }
--- Widget.xaml.cs
+++ Widget.xaml.cs
@@ -3,11 +3,11 @@
 namespace Maeily_Windows
 {
     /// <summary>
-    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// Widget.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Widget : Window
     {
-        public MainWindow()
+        public Widget()
         {
             InitializeComponent();
         }
