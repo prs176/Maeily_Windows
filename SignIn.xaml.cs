b668555dd94a5b88941f3141e61605497d3c3e55
prs176 <>
2020-09-15 20:44:35 +0900

    1

--- SignIn.xaml.cs
+++ SignIn.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Maeily_Windows
 {
@@ -10,6 +12,32 @@ namespace Maeily_Windows
         public SignIn()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            TextBox txtbox = sender as TextBox;
+            txtbox.SelectionStart = 0;
+        }
+        
+        private void TextBox_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            char keyCode = (char)e.Key;
+            if (!char.IsDigit(keyCode))
+            {
+
+            }
+
+            TextBox txtbox = sender as TextBox;
+            if (string.Compare(txtbox.Text, "아이디를 입력해주세요") == 0 || string.Compare(txtbox.Text, "비밀번호를 입력해주세요") == 0)
+            {
+                txtbox.Text = "";
+            }
+        }
+
+        private void TextBox_MouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+
         }
     }
 }
